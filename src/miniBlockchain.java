ce44899a61a3ad75b2462d9377738cf372d2f22f
JoaquinCar <joaquincarmona758@gmail.com>
2025-03-24 22:51:21 -0600

    json print

--- src/miniBlockchain.java
+++ src/miniBlockchain.java
@@ -4,6 +4,14 @@ public class miniBlockchain{
 
     public static ArrayList<block> blockchain = new ArrayList<block>();
     public static void main(String[] args) {
+        blockchain.add(new block("Hi im the first block", "0"));
+        blockchain.add(new block("Hi im the second block", blockchain.get(blockchain.size()-1).hash));
+        blockchain.add(new block("Hi im the third block", blockchain.get(blockchain.size()-1).hash));
+
+        String blockchainJson = new GsonBuilder().setPrettyPrinting().create().toJson(blockchain);
+        System.out.println(blockchainJson);
+
+
 
 
     }
